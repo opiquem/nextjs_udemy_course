1eff960fc6e6b88b96a3a242e197a5ab02b9c3b1
opiquem <booobryyyk@gmail.com>
2024-03-28 23:20:25 +0200

    initial commit from prisma installing lesson

---
+++ app/reviews/[slug]/page.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from 'next';
+import { ChatBubbleBottomCenterTextIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import CommentForm from '@/components/CommentForm';
+import CommentList from '@/components/CommentList';
+import Heading from '@/components/Heading';
+import ShareLinkButton from '@/components/ShareLinkButton';
+import { getReview, getSlugs } from '@/lib/reviews';
+
+interface ReviewPageParams {
+  slug: string;
+}
+
+interface ReviewPageProps {
+  params: ReviewPageParams;
+}
+
+export async function generateStaticParams(): Promise<ReviewPageParams[]> {
+  const slugs = await getSlugs();
+  // console.log('[ReviewPage] generateStaticParams:', slugs);
+  return slugs.map((slug) => ({ slug }));
+}
+
+export async function generateMetadata({ params: { slug } }: ReviewPageProps): Promise<Metadata> {
+  const review = await getReview(slug);
+  if (!review) {
+    notFound();
+  }
+  return {
+    title: review.title,
+  };
+}
+
+export default async function ReviewPage({ params: { slug } }: ReviewPageProps) {
+  console.log('[ReviewPage] rendering', slug);
+  const review = await getReview(slug);
+  if (!review) {
+    notFound();
+  }
+  return (
+    <>
+      <Heading>{review.title}</Heading>
+      <p className="font-semibold pb-3">
+        {review.subtitle}
+      </p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+      </div>
+      <Image src={review.image} alt="" priority
+        width="640" height="360" className="mb-2 rounded"
+      />
+      <article dangerouslySetInnerHTML={{ __html: review.body }}
+        className="max-w-screen-sm prose prose-slate"
+      />
+      <section className="border-dashed border-t max-w-screen-sm mt-3 py-3">
+        <h2 className="font-bold flex gap-2 items-center text-xl">
+          <ChatBubbleBottomCenterTextIcon className="h-6 w-6" />
+          Comments
+        </h2>
+        <CommentForm title={review.title} />
+        <CommentList />
+      </section>
+    </>
+  );
+}
---
+++ lib/reviews.ts
@@ -0,0 +1,110 @@
+import 'server-only';
+
+import { marked } from 'marked';
+import qs from 'qs';
+
+export const CACHE_TAG_REVIEWS = 'reviews';
+
+const CMS_URL = process.env.CMS_URL;
+
+interface CmsItem {
+  id: number;
+  attributes: any;
+}
+
+export interface Review {
+  slug: string;
+  title: string;
+  subtitle: string;
+  date: string;
+  image: string;
+}
+
+export interface FullReview extends Review {
+  body: string;
+}
+
+export interface PaginatedReviews {
+  pageCount: number;
+  reviews: Review[];
+}
+
+export type SearchableReview = Pick<Review, 'slug' | 'title'>;
+
+export async function getReview(slug: string): Promise<FullReview | null> {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+    populate: { image: { fields: ['url'] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  if (data.length === 0) {
+    return null;
+  }
+  const item = data[0];
+  return {
+    ...toReview(item),
+    body: marked(item.attributes.body),
+  };
+}
+
+export async function getReviews(pageSize: number, page?: number): Promise<PaginatedReviews> {
+  const { data, meta } = await fetchReviews({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize, page },
+  });
+  return {
+    pageCount: meta.pagination.pageCount,
+    reviews: data.map(toReview),
+  };
+}
+
+export async function searchReviews(query: string): Promise<SearchableReview[]> {
+  const { data } = await fetchReviews({
+    filters: { title: { $containsi: query } },
+    fields: ['slug', 'title'],
+    sort: ['title'],
+    pagination: { pageSize: 5 },
+  });
+  return data.map(({ attributes }) => ({
+    slug: attributes.slug,
+    title: attributes.title,
+  }));
+}
+
+export async function getSlugs(): Promise<string[]> {
+  const { data } = await fetchReviews({
+    fields: ['slug'],
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 100 },
+  });
+  return data.map((item: CmsItem) => item.attributes.slug);
+}
+
+async function fetchReviews(parameters: any) {
+  const url = `${CMS_URL}/api/reviews?`
+    + qs.stringify(parameters, { encodeValuesOnly: true });
+  // console.log('[fetchReviews]:', url);
+  const response = await fetch(url, {
+    next: {
+      tags: [CACHE_TAG_REVIEWS],
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return await response.json();
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    subtitle: attributes.subtitle,
+    date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
+    image: new URL(attributes.image.data.attributes.url, CMS_URL).href,
+  };
+}
