0a81f1ce18743a8802a38faf750934cc32bf4112
opiquem <booobryyyk@gmail.com>
2024-04-09 14:03:25 +0300

    fix: changed body background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={`${exo2.variable} ${orbitron.variable}`}>
-      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
+      <body className="bg-orange-100 flex flex-col px-4 py-2 min-h-screen">
         <header>
           <NavBar />
         </header>
